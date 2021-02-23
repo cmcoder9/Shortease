731621fd6c462b0ed08c970a646a04e7047009bc
cmcoder9 <cmcoder@gmail.com>
2021-02-22 22:10:16 -0800

    all csv and flask update

--- app.py
+++ app.py
@@ -1,16 +1,11 @@
-from dotenv import load_dotenv
-from flask import (
-    Flask,
-    render_template,
-    jsonify,
-    request,
-    redirect)
+# from dotenv import load_dotenv
+from flask import Flask, render_template, jsonify, request, redirect
 
 import scrape_wsj
 import sys
 
 #load enviroment variables
-load_dotenv
+# load_dotenv
 ##########################################
 # Flask Setup
 ##########################################
@@ -28,55 +23,55 @@ mongo = PyMongo(app)
 def home():
 
      # Open connection // flack-pymongo nannages connections 
-    mongo = PyMongo(app)
+    # mongo = PyMongo(app)
     
     # Find record of data from the mongo database
-    wsj = mongo.db.wsj_data.find_one()
+    wsj = mongo.db.wsj.find_one()
     
     # Construct array 
-    wsj = [x for x in wsj]
+#    wsj = [x for x in wsj]
     
     # Get randon index
-    randon_select = np.random.randint( low=0, high=3)
+#     randon_select = np.random.randint( low=0, high=3)
     
     # Use random index to selct a doc
-    wsj = wsj[randon_select]
-    
+#    wsj = wsj[randon_select]
+    return jsonify(wsj['0'])
     # Return template and data
-    return render_template("index.html", shortease_html= wsj)
+   # return render_template("index.html", shortease_html= wsj)
 
 #create routes for data
 @app.route("/GMEData")
 def my_data():
      
      # Open connection   
-    mongo = PyMongo(app)
+    # mongo = PyMongo(app)
     
     # Pull down doc from mongoDB
-    my_query = mongo.db.wsj_data.find()
+    #my_query = mongo.db.wsj_data.find()
     
     # Construct array
-    my_data = [x for x in my_query]
+    #my_data = [x for x in my_query]
     
     # Return array
-    return jsonify(my_data)
-
+    #return jsonify(my_data)
+    return ""
 
 # Route that will trigger the scrape function
 @app.route("/scrape")
 def scrape():
 
     # Run the scrape function
-    wsj = mongo.db.wsj
-    wsj_data = scrape_wsj.scrape_all()
-    print(wsj)
+   # wsj = mongo.db.wsj
+   # wsj_data = scrape_wsj.scrape_all()
+   # print(wsj)
     
     # Update the Mongo database using update and upsert=True
-    mongo.db.collection.update({}, wsj_data, upsert=True)
+   # mongo.db.collection.update({}, wsj_data, upsert=True)
 
     # Redirect back to home page
-    return redirect("/", code=302)
-
+    #return redirect("/", code=302)
+    return ""
 
 if __name__ == "__main__":
     app.run(debug=True)
